cb7a542fbae75502597ebb8496d1028cffdb2052
zeruel11 <the.zero0110@gmail.com>
2019-03-31 17:43:03 +0700

    updated alias sh

--- cmder_config/profile.d/aliases.sh
+++ cmder_config/profile.d/aliases.sh
@@ -3,5 +3,17 @@ alias ll='ls -ltra'
 alias la='ls -la'
 alias pwdw='cygpath -ml `pwd`'
 alias docs='cd $HOMEDRIVE/Documents'
+
+# git
 alias gf='git fetch --all'
 alias gs='git status'
+alias gc='git checkout'
+alias gm='git commit -am'
+alias gl='git log --oneline --all --graph --decorate'
+alias g1='git log -1'
+alias gt='git tag -am'
+alias gcp='git cherry-pick -e'
+alias gcx='git cherry-pick -e --strategy=recursive -X theirs'
+
+# ikti server
+alias ikti.srv='ssh bambang_ebis@10.126.12.212 -i /c/Users/zeruel11/.ssh/iktisrv_id'
