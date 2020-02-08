e8626c772bc7fab4fb5dfa7fe99300c35fc04dfe
zeruel11 <the.zero0110@gmail.com>
2020-02-09 06:44:29 +0700

    updated

--- cmder_config/user_profile.ps1
+++ cmder_config/user_profile.ps1
@@ -195,3 +195,23 @@ $c = [ConsoleColor]::Cyan
 $b = [ConsoleColor]::Gray
 $h = [ConsoleColor]::DarkGray
 ###############################################################################
+
+function U
+{
+    param
+        (
+	        [int] $Code
+		)
+		     
+		    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+			{
+			             return [char] $Code
+				         }
+					  
+					      if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+					          {
+						          return [char]::ConvertFromUtf32($Code)
+							      }
+							       
+							           throw "Invalid character code $Code"
+								   }
