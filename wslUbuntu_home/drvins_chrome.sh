a3653ed8d0c05126781745245e5a8deb47c28296
zeruel11 <the.zero0110@gmail.com>
2020-02-25 01:07:05 +0700

    Initial commit bucket

--- wslUbuntu_home/drvins_chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# platform options: linux32, linux64, mac64, win32
-PLATFORM=linux64
-VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-curl http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip | bsdtar -xvf -
-chmod u+xw,g+x-w,o+x chromedriver
